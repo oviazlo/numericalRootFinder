162efeab9a4c38ac7cd2ec038f9bc4e9d56c3303
Oleksandr Viazlo <Oleksandr.Viazlo@cern.ch>
2017-08-25 13:12:26 +0200

    add drawing of the plot

--- NumericalRootFinder2.cpp
+++ NumericalRootFinder2.cpp
@@ -6,21 +6,16 @@
 
 using namespace std;
 
-double k = -0.1;
-double tUnivTimesH0 = 0.908562;
+double tUniv_years = 12.7-0.7;
+double tUniv_seconds = tUniv_years*365*24*60*60*1.0e+09;
+double H0_seconds = 2.26853e-18;
+double tUnivTimesH0 = tUniv_seconds*H0_seconds;
+double k = 0.0;
 
-// error on Universe age:
-// 0.500782
-
-// double myfunc(double x ) {
-//    return k*x*x-1;
-// }
- 
 double myfunc(double x) { 
-   // double xMat = 1.0 + k - x;
-   // double xRad = x;
-   double xMat = x;
-   double xRad = 1.0 + k - x;
+   double xMat = 1.0 + k - x;
+   double xRad = x;
+
    double numeratorLog = xMat + 2.0*sqrt( (-k)*xRad );
    double denominatorLog = 2*(-k)+xMat+2*sqrt(xRad);
    double logCoef = xMat / ( 2 * pow((-k),3.0/2.0) );
@@ -28,60 +23,66 @@ double myfunc(double x) {
    return (logCoef * TMath::Log( numeratorLog / denominatorLog ) + leftover) - tUnivTimesH0; 
 }
 
-// double myfunc_deriv ( double x ) {
-//    return 2*k*x;
-// }
-
 int NumericalRootFinder2()
 {
  
-   const int nPoints = 1;
    TCanvas *can = new TCanvas("can","can",800,600);
-   TLegend *leg = new TLegend(0.3,0.65,0.9,0.9);
-   TF1 *f[nPoints];
-   for (int i=0; i<nPoints; i++){
-	   k = 0.01+i*0.98/nPoints;
 
-	   // Create the function and wrap it
-	   f[i] = new TF1("myfunc", "myfunc(x)", 0.0,1.0+k);
-	   ROOT::Math::WrappedTF1 wf1(*f[i]);
-	 
-	   // Create the Integrator
-	   ROOT::Math::BrentRootFinder brf;
-	 
-	   // Set parameters of the method
-	   brf.SetFunction( wf1, 0.0, 1.0+k );
-	   brf.Solve();
-	 
-	   // cout << "k = " << k << "; root = " << brf.Root() << "; " ;
-	   // cout << "sqrt(1/k) = " << TMath::Sqrt(1/k) << endl;
-	    
-	   // cout << "Omega0 = " << 1+k << "; fract = " << brf.Root()/(1+k) << "; root = " << brf.Root() << endl;
-	   cout << "Omega0 = " << 1+k << "; xMat = " << brf.Root() << "; xRad = " << 1+k-brf.Root() << endl;
+   const int nPoints = 1000;
 
-	   f[i]->SetLineColor(i+1);
-	   const int nLinesToDraw = 10;
-	   if (i==0){
-		   f[i]->Draw("A");
-		   leg->AddEntry(f[i],("k = " + to_string(k)).c_str());
+   double curv[nPoints];
+   double xRad[3][nPoints];
+   double xMat[3][nPoints];
+   double tUniverse[3] = {12.0,12.7,13.4};
+
+   for (int kk=0; kk<3; kk++){
+	   tUniv_years = tUniverse[kk];
+	   double tUniv_seconds = tUniv_years*365*24*60*60*1.0e+09;
+	   double H0_seconds = 2.26853e-18;
+	   tUnivTimesH0 = tUniv_seconds*H0_seconds;
+	   for (int i=0; i<nPoints; i++){
+		   k = -(0.00001+i*0.99999/nPoints);
+
+		   // Create the function and wrap it
+		   TF1* f = new TF1("myfunc", "myfunc(x)", 0.0,1.0+k);
+		   ROOT::Math::WrappedTF1 wf1(*f);
+		 
+		   // Create the Integrator
+		   ROOT::Math::BrentRootFinder brf;
+		 
+		   // Set parameters of the method
+		   brf.SetFunction( wf1, 0.0, 1.0+k );
+		   brf.Solve();
+		 
+		   cout << "Omega0 = " << 1+k << "; xMat = " << brf.Root() << "; xRad = " << 1+k-brf.Root() << endl;
+		   curv[i] = 1+k;
+		   xMat[kk][i] = brf.Root();
+		   xRad[kk][i] = 1+k-brf.Root();
+
+	   } 
+   }
+
+   TGraph* grRad[3];
+   TGraph* grMat[3];
+   for (int kk=0; kk<3; kk++){
+	   grRad[kk] = new TGraph(nPoints, curv, xRad[kk]);
+	   grRad[kk]->SetMarkerColor(kRed);
+	   grRad[kk]->SetLineColor(kRed);
+	   if (kk==0){
+		   grRad[kk]->Draw("AP");
+		   grRad[kk]->GetXaxis()->SetTitle("1-#Omega_{0}");
+		   grRad[kk]->SetTitle("");
 	   }
-	   else if (i<=nLinesToDraw){
-		   f[i]->Draw("same");
-		   leg->AddEntry(f[i],("k = " + to_string(k)).c_str());
-		   if (i==nLinesToDraw || i==(nPoints-1)){
-			   leg->Draw("same");
-			   can->SaveAs("test2.png");
-		   }
-	   }
-	   // if (i==0){
-	   //         can->Print("test2.pdf[");
-	   //         can->SaveAs("test2.png");
-	   // }
-	   // else if (i==(nPoints-1))
-	   //         can->Print("test2.pdf]");
-	   // else
-	   //         can->Print("test2.pdf");
-   } 
+	   else
+		   grRad[kk]->Draw("P same");
+	   grMat[kk] = new TGraph(nPoints, curv, xMat[kk]);
+	   grMat[kk]->Draw("P same");
+   }
+   TLegend *leg = new TLegend(0.12,0.7,0.32,0.9);
+   leg->AddEntry(grRad[0],"#Omega_{r}","l");
+   leg->AddEntry(grMat[0],"#Omega_{m}","l");
+   leg->Draw("same");
+   can->SaveAs("plot.png");
 
    return 0;
 }
